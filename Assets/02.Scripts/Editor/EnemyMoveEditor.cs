4423220725c3569d19e59c20b18f38ab4fd243ba
kungsae <68137693+kungsae@users.noreply.github.com>
2021-10-22 13:48:24 +0900

    타일맵 찍는중 에너미 패트롤 포인트 에디터 안뜨는 버그 수정

--- Assets/02.Scripts/Editor/EnemyMoveEditor.cs
+++ Assets/02.Scripts/Editor/EnemyMoveEditor.cs
@@ -1,18 +1,17 @@
 using UnityEngine;
 using UnityEditor;
 
-[CustomEditor(typeof(EnemyControl))]
+[CustomEditor(typeof(EnemyControl),true)]
 public class EnemyMoveEditor : Editor
 {
 	private void OnSceneGUI()
 	{
 		EnemyControl enemy = (EnemyControl)target;
-
+		Handles.color = Color.red;
 		if (enemy.patrollPoint.Length > 0)
 		{
 			for (int i = 0; i < enemy.patrollPoint.Length; i++)
 			{
-				Handles.color = Color.red;
 				Handles.DrawWireCube(enemy.patrollPoint[i], new Vector3(1, 1));
 			}
 		}
